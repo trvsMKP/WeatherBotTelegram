9274748a72e1d741e504e605955bbf360a7d7274
Vladislav <travus.mckeep@gmail.com>
2022-02-23 16:56:45 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,2 @@
+open_weather_token = "20908340c75b1c1e809880056a2bc217"
+tg_bot_token = "5125068289:AAG8FvBvqTZEGAn17uC-Mb5qppVAzBx-HuQ"
---
+++ server.bat
@@ -0,0 +1,4 @@
+@echo off
+
+python main_weather_tg_bot.py
+pause
